26a5de1d1893988f2cc40560f699dea4668c6616
natrix_dev <discordlenko@gmail.com>
2022-08-22 11:27:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print('Hello, please provide the following information: ')
 print('')
-name = input('Your Name: ')
-age = input('You Age: ')
-job = input('You Job: ')
+name = input('Your Name: ') # asking for name
+age = input('You Age: ') # asking for age 
+job = input('You Job: ') # asking for job
 print('')
-print('Hi, '+str(name)+' who works as a ' +str(job)+ ' and is '+str(age)+' years old ')
+print('Hi, '+str(name)+' who works as a ' +str(job)+ ' and is '+str(age)+' years old ') # print all the user informations
